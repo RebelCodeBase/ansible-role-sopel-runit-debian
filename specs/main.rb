25da2d2d4298466e7dfa147d5deac44195ba8549
RebelCodeBase <rebelcode@riseup.net>
2016-06-06 23:17:52 +0200

    Add user serverspec tests

--- specs/main.rb
+++ specs/main.rb
@@ -3,3 +3,17 @@ require 'spec_helper'
 describe package 'runit' do
   it { should be_installed }
 end
+
+describe user 'sopel' do
+  it { should exist }
+  it { should belong_to_group 'sopel' }
+  it { should have_home_directory '/home/sopel' }
+  it { should have_login_shell '/bin/false' }
+end
+
+describe user 'sopel-log' do
+  it { should exist }
+  it { should belong_to_group 'sopel' }
+  it { should have_home_directory '' }
+  it { should have_login_shell '/bin/false' }
+end
