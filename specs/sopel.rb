b4145364ba6095aba09357733ff58d13e652d301
RebelCodeBase <rebelcode@riseup.net>
2016-06-09 05:01:56 +0200

    Add more serverspec tests

--- specs/sopel.rb
+++ specs/sopel.rb
@@ -14,6 +14,7 @@ botdir = "#{homedir}/sopel"
 moddir = "#{homedir}/modules"
 configdir = "#{homedir}/.sopel"
 custommoddir = "#{configdir}/modules"
+configfile = 'default.cfg'
 
 dirs = [ homedir, botdir, moddir, configdir, custommoddir ]
 
@@ -26,3 +27,41 @@ dirs.each do | dir |
   end
 end
 
+git_remote_botdir = "git --git-dir=#{botdir}/.git remote --verbose | grep fetch | cut -d ' ' -f 1 | cut -f 2"
+describe "Git remote \"#{botdir}/.git\"" do
+  it 'should be https://github.com/sopel-irc/sopel.git' do
+    sopel_git_remote_botdir = command(git_remote_botdir).stdout.chomp
+    expect(sopel_git_remote_botdir).to eq 'https://github.com/sopel-irc/sopel.git'
+  end
+end
+
+describe file "#{botdir}/sopel.py" do
+  it { should exist }
+  it { should be_file }
+  it { should be_executable }
+  it { should be_owned_by "#{pname}" }
+  it { should be_grouped_into "#{pname}" }
+  it { should be_mode 744 }
+end
+
+git_remote_rssdir = "git --git-dir=#{moddir}/sopel-rss/.git remote --verbose | grep fetch | cut -d ' ' -f 1 | cut -f 2"
+describe "Git remote \"#{moddir}/sopel-rss/.git\"" do
+  it 'should be https://github.com/rebelcodebase/sopel-rss.git' do
+    sopel_git_remote_rssdir = command(git_remote_rssdir).stdout.chomp
+    expect(sopel_git_remote_rssdir).to eq 'https://github.com/rebelcodebase/sopel-rss.git'
+  end
+end
+
+describe file "#{custommoddir}/rss.py" do
+  it { should exist }
+  it { should be_symlink }
+  it { should be_linked_to "#{moddir}/sopel-rss/rss.py" }
+end
+
+describe file "#{configdir}/#{configfile}" do
+  it { should exist }
+  it { should be_file }
+  it { should be_owned_by "#{pname}" }
+  it { should be_grouped_into "#{pname}" }
+  it { should be_mode 644 }
+end
